96f05ce3726880027f1fb6928d6221e6fdac3058
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2023-04-22 18:22:34 +0900

    [add] atoms追加’

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,25 +1,13 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-
-import { Home } from "./Home";
-import { Page01 } from "./Page01";
-import { Page02 } from "./Page02";
+import { PrimaryButton  } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import './style.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/Page01">Page01</Link>
-        <br />
-        <Link to="/Page02">Page02</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="page01" element={<Page01 />} />
-        <Route path="page02" element={<Page02 />}  />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+    </>
   );
 }
 
