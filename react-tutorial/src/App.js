40693c214eb7f865c1a982920beb091263cca66b
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2023-04-24 21:35:36 +0900

    [fix] 三目並べ’

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+
+const Square = ({ value }) => {
+  return <button className="square">{value}</button>;
+}
+
+
+const Board = () => {
+  return (
+    //フラグメントでラップしiないといけない
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
+
+export default Board;
+//メイン関数であり、外部からimport可能
