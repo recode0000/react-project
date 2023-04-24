14bdc6976fb3cb8089b071efedf7a587fa346ba6
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2023-04-24 21:40:44 +0900

    [add] 調整

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 
-const Square = ({ value }) => {
+const Square = () => {
+  const [value, setValue] = useState(null);
+  const handleClick = () => {
+    setValue(value);
+  }
   return <button className="square">{value}</button>;
 }
 
@@ -11,19 +15,19 @@ const Board = () => {
     //フラグメントでラップしiないといけない
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
