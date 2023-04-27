5a4ce7e01e3442a3d425dce7b7e2a5c22368e5ce
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2023-04-28 08:44:34 +0900

    [fix] Todoアプリ バージョン依存

--- chatgpt_redux/src/App.js
+++ chatgpt_redux/src/App.js
@@ -1,33 +1,14 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./features/counter/counterSlide";
+import React from 'react';
+import { Provider } from 'react-redux';
+import TodoList from './components/TodoList';
+import store from './store';
 
-function App() {
-  const [incrementAmount, setIncrementAmount] = useState("2");
-  const count = useSelector(state => state.counter.value);
-  const dispatch = useDispatch();
-
-  const incrementValue = Number(incrementAmount) || 0;
-
+const App = () => {
   return (
-    <div>
-      <dvi>
-        <button onClick={() => dispatch(decrement())}></button>
-        <span>{count}</span>
-        <button onClick={() => dispatch(increment())}></button>
-      </dvi>
-      <div>
-        <input
-          type="text"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button onClick={() => dispatch(increment(incrementValue))}>
-          Add Amount
-        </button>
-      </div>
-    </div>
-  );
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  )
 }
 
 export default App;
---
+++ chatgpt_redux/src/TodoList.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask, deleteTask, toggleTask } from '../actions';
+
+const TodoList = () => {
+  const [newTask, setNewTask] = useState('');
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks);
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    dispatch(addTask({ id: Date.now(), name: newTask }));
+    setNewTask('');
+  }
+
+  const handleDeleteTask = (taskId) => {
+    dispatch(deleteTask(taskId));
+  }
+
+  const handleToggleTask = (taskId) => {
+    dispatch(toggleTask(taskId));
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleAddTask}>
+        <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input type="checkbox" checked={task.completed} onChange={() => handleToggleTask(task.id)} />
+            <span style={{ textDecoration: task.completed ? 'line-through': 'none'}}>{task.name}</span>
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+
+export default TodoList;
