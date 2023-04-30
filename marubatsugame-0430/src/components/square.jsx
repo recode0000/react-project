64d11968ebf7e7e27518a8cc6925f8112186cf3a
Rikito Ikeda <rikito1036@icloud.com>
2023-04-30 13:09:16 +0900

    [fix] 完成

--- marubatsugame-0430/src/components/square.jsx
+++ marubatsugame-0430/src/components/square.jsx
@@ -1,5 +1,15 @@
-const Square = ({ value }) => {
-  return <button className="square">{ value }</button>;
+// import { useState } from "react";
+
+const Square = ({value}) => {
+  // const [value, setValue] = useState(null);
+
+  // const handleClick = () => {
+  //   setValue('X');
+  // }
+  return (
+    // <div className="square" onClick={handleClick}>{value}</div>
+    <div className="square">{value}</div>
+  );
 }
 
 export default Square;
