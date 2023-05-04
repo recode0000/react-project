d12ac54ecc4bd1fcf8fcf163e7403a08b0244860
Rikito Ikeda <rikito1036@icloud.com>
2023-05-04 19:27:26 +0900

    [fix] 変更箇所のみレンダリング&

--- react-docs/src/App.js
+++ react-docs/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  const element = <h1>Hello World</h1>
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
 
-    </div>
-  );
-}
-
-export default App;
+// export default App;
 
 
 // /React はマークアップとロジックを両方含む疎結合の「コンポーネント」という単位を用いて関心を分離します。
-/
+//
+
+//Reactの要素はイミュータブル
+//Reactの要素はイミュータブル
