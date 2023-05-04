5316ea7d0193c6332d7083b29cae39bf504e2e84
Rikito Ikeda <rikito1036@icloud.com>
2023-05-04 19:08:50 +0900

    [fix] 入門編

---
+++ react-docs/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const element = <h1>Hello World</h1>
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
+
+// /React はマークアップとロジックを両方含む疎結合の「コンポーネント」という単位を用いて関心を分離します。
+/
