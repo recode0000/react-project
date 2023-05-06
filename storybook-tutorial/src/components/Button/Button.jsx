1ceeb50329298ac7c830a8c5bc8851541065da9c
Rikito Ikeda <rikito1036@icloud.com>
2023-05-06 23:29:49 +0900

    [fix] ファイル削除&

--- storybook-tutorial/src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import "./button.css";
-
-const Button = ({ children, color="default", size='base'}) => {
-  return <button className={`${color} ${size}`}>{children}</button>;
-};
-
-export default Button;
