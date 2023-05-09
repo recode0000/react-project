ebf6a1aeae0b943576512b52a5fd5f94e95951e8
Rikito Ikeda <rikito1036@icloud.com>
2023-05-09 09:06:09 +0900

    [add] 天気アプリケーション Tailwind.cssを追加

---
+++ tailwindcss_weather/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      Weather Application
+    </div>
+  );
+}
+
+export default App;
