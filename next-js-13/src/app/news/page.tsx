d2a25688c469ebb4065ba97a468de65c77283916
Rikito Ikeda <rikito1036@icloud.com>
2023-05-13 13:52:41 +0900

    nextjs13の基本機能

---
+++ next-js-13/src/app/news/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>News</div>
+    <Link href="/users">User</Link>
+    </>
+  )
+}
+
+export default page
